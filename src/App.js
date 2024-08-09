7228e66e671ef76ca6ef0b369ccf3102f7b12c2b
WeeeFamily <maaxsvt@gmail.com>
2024-08-09 10:31:22 -0800

    firstCommit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./Components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
 import ProductList from "./Components/ProductList/ProductList";
 import Form from "./Components/Form/Form";
+const tg = window.Telegram.WebApp;
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
@@ -14,21 +15,10 @@ function App() {
         tg.ready();
 
     }, [])
-///
-
-
-
-
-
-
-
 
     return (
         <div className="App">
             <Header />
-
-
-
             <Routes>
                 <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
